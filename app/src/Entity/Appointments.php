1c2d0f11659121d7a73a3ee97878fb006ee3aca6
ASavastru <smeag0leye>
2022-09-04 16:28:32 +0300

    Database & Entities

---
+++ app/src/Entity/Appointments.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AppointmentsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AppointmentsRepository::class)]
+class Appointments
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $Date = null;
+
+    #[ORM\Column]
+    private ?int $Location_ID = null;
+
+    #[ORM\Column]
+    private ?int $User_ID = null;
+
+    #[ORM\ManyToOne(targetEntity: User::class, fetch: 'EXTRA_LAZY', inversedBy: 'appointments')]
+    #[ORM\JoinColumn(name: 'user_id', referencedColumnName: 'id', nullable: false)]
+    protected User $user;
+
+    #[ORM\ManyToOne(targetEntity: Locations::class, fetch: 'EXTRA_LAZY', inversedBy: 'appointments')]
+    #[ORM\JoinColumn(name: 'location_id', referencedColumnName: 'id', nullable: false)]
+    protected Locations $location;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->Date;
+    }
+
+    public function setDate(\DateTimeInterface $Date): self
+    {
+        $this->Date = $Date;
+
+        return $this;
+    }
+
+    public function getLocationID(): ?int
+    {
+        return $this->Location_ID;
+    }
+
+    public function setLocationID(int $Location_ID): self
+    {
+        $this->Location_ID = $Location_ID;
+
+        return $this;
+    }
+
+    public function getUserID(): ?int
+    {
+        return $this->User_ID;
+    }
+
+    public function setUserID(int $User_ID): self
+    {
+        $this->User_ID = $User_ID;
+
+        return $this;
+    }
+}
---
+++ app/src/Entity/Locations.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LocationsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LocationsRepository::class)]
+class Locations
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $City = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Address = null;
+
+    #[ORM\Column]
+    private ?int $Capacity = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->City;
+    }
+
+    public function setCity(string $City): self
+    {
+        $this->City = $City;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->Address;
+    }
+
+    public function setAddress(string $Address): self
+    {
+        $this->Address = $Address;
+
+        return $this;
+    }
+
+    public function getCapacity(): ?int
+    {
+        return $this->Capacity;
+    }
+
+    public function setCapacity(int $Capacity): self
+    {
+        $this->Capacity = $Capacity;
+
+        return $this;
+    }
+}
